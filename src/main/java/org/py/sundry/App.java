1b9e536b3a49d37524c4d5a4978cf5e748a0bb7c
pythagoras <magicianhy@126.com>
2019-01-22 10:06:20 +0800

    initialize

---
+++ src/main/java/org/py/sundry/App.java
@@ -0,0 +1,13 @@
+package org.py.sundry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
