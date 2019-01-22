600bfd634c336a760fe014c7fad8cc0f23ae0a55
pythagoras <magicianhy@126.com>
2019-01-22 11:34:11 +0800

    change

--- src/main/java/org/py/sundry/ApplicationSundry.java
+++ src/main/java/org/py/sundry/ApplicationSundry.java
@@ -1,5 +1,11 @@
 package org.py.sundry;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApplicationSundry {
-	
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationSundry.class, args);
+	}
 }
